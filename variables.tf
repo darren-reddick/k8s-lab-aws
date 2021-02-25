4cb5d18fcf415d9b0e3c429a7ffbea2fcf37394c
dreddick.home2@gmail.com <dreddick.home2@gmail.com>
2021-02-25 19:37:28 +0000

    add support for version variable

--- variables.tf
+++ variables.tf
@@ -5,3 +5,13 @@ variable "region" {
 variable "environment" {
   default = "devops"
 }
+
+variable "k8s_version" {
+  default = "1.19.8-00"
+  description = "The kubernetes version identifier"
+
+  validation {
+    condition     = contains(["1.19.8-00","1.20.0-00"],var.k8s_version)
+    error_message = "The k8s_version specified has not been tested.\nThis can be overridden by updating the condition in the validation for k8s_version."
+  }
+}
